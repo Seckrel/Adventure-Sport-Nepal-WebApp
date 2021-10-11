e5aa0dcf2420a381a7f2051f3895850c6bf9d76f
Aayam <aayam.oza@gmail.com>
2021-10-11 15:05:31 +0545

    force replace menuitems.tsx

--- adventureSportNepal/frontend/src/header/components/MenuItems.ts
+++ adventureSportNepal/frontend/src/header/components/MenuItems.ts
@@ -1,153 +1,41 @@
-import { NavigationItemType } from '../types.d';
-
 export const AboutUs = [
     {
         name: "Our Team",
         href: '',
-        items: null
     },
     {
         name: "Saftey",
         href: '',
-        items: null
     },
 ]
 
-// export const Expeditions = [
-//     {
-//         name: "COMBO",
-//         href: '',
-//         items: null
-//     },
-//     {
-//         name: "SKI",
-//         items: [
-//             {
-//                 name: "Ski in Puthan Hinchuli (7426m)",
-//                 href: '',
-//                 items: null
-//             },
-//             {
-//                 name: "Ski in  Mera Peak (6654m)",
-//                 href: '',
-//                 items: null
-//             },
-//             {
-//                 name: "Ski in Bhrikuti Peak (6361m)",
-//                 href: '',
-//                 items: null
-//             },
-//             {
-//                 name: "Ski in Ramdung Go Peak (5925m)",
-//                 href: '',
-//                 items: null
-//             },
-//             {
-//                 name: "Ski in Kalinchowk (3842m)",
-//                 href: '',
-//                 items: null
-//             }
-//         ]
-//     },
-//     {
-//         name: "mountainering",
-//         items: [
-//             {
-//                 name: "Chulu West Peak Climbing (6419m)",
-//                 href: '',
-//                 items: null
-//             },
-//             {
-//                 name: "Island Peak Climbing (6189m)",
-//                 href: '',
-//                 items: null
-//             },
-//             {
-//                 name: "Manaslu Peak Climbing (8163m)",
-//                 href: '',
-//                 items: null
-//             },
-//             {
-//                 name: "Mera Peak Peak Climbing (6461m)",
-//                 href: '',
-//                 items: null
-//             },
-//             {
-//                 name: "Yala Peak Climbing (5520m)",
-//                 href: '',
-//                 items: null
-//             },
-//         ]
-//     },
-//     {
-//         name: "Trekking",
-//         items: [
-//             {
-//                 name: "Annapurna Circuit Trek",
-//                 href: '',
-//                 items: null
-//             },
-//             {
-//                 name: "Dhaulagiri Circuit Trek",
-//                 href: '',
-//                 items: null
-//             },
-//             {
-//                 name: "Everest Circuit Trek",
-//                 href: '',
-//                 items: null
-//             },
-//             {
-//                 name: "Guerilla Circuit Trek",
-//                 href: '',
-//                 items: null
-//             },
-//             {
-//                 name: "Manaslu Circuit Trek",
-//                 href: '',
-//                 items: null
-//             },
-//         ]
-//     }
-
-// ]
-
-
-
-
-export const Expeditions: NavigationItemType[] = [
+export const Expeditions = [
     {
         name: "COMBO",
-        items: null
+        href: '',
     },
     {
-        name: "SKING",
+        name: "SKI",
         items: [
             {
                 name: "Ski in Puthan Hinchuli (7426m)",
                 href: '',
-                items: null
-
             },
             {
                 name: "Ski in  Mera Peak (6654m)",
                 href: '',
-                items: null
             },
             {
                 name: "Ski in Bhrikuti Peak (6361m)",
                 href: '',
-                items: null
             },
             {
                 name: "Ski in Ramdung Go Peak (5925m)",
                 href: '',
-                items: null
             },
             {
                 name: "Ski in Kalinchowk (3842m)",
                 href: '',
-                items: null
             }
         ]
     },
@@ -157,27 +45,22 @@ export const Expeditions: NavigationItemType[] = [
             {
                 name: "Chulu West Peak Climbing (6419m)",
                 href: '',
-                items: null
             },
             {
                 name: "Island Peak Climbing (6189m)",
                 href: '',
-                items: null
             },
             {
                 name: "Manaslu Peak Climbing (8163m)",
                 href: '',
-                items: null
             },
             {
                 name: "Mera Peak Peak Climbing (6461m)",
                 href: '',
-                items: null
             },
             {
                 name: "Yala Peak Climbing (5520m)",
                 href: '',
-                items: null
             },
         ]
     },
@@ -187,29 +70,23 @@ export const Expeditions: NavigationItemType[] = [
             {
                 name: "Annapurna Circuit Trek",
                 href: '',
-                items: null
             },
             {
                 name: "Dhaulagiri Circuit Trek",
                 href: '',
-                items: null
             },
             {
                 name: "Everest Circuit Trek",
                 href: '',
-                items: null
             },
             {
                 name: "Guerilla Circuit Trek",
                 href: '',
-                items: null
             },
             {
                 name: "Manaslu Circuit Trek",
                 href: '',
-                items: null
             },
         ]
     }
-
 ]
