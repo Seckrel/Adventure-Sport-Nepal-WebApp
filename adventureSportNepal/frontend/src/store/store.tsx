18897540203447c507b5b0b5985b65e688cd61a0
Aayam <aayam.oza@gmail.com>
2021-10-12 15:02:55 +0545

    Maximum items to show in navigation has been added i.e. MAX_NAV_ITEMS

--- adventureSportNepal/frontend/src/store/store.tsx
+++ adventureSportNepal/frontend/src/store/store.tsx
@@ -5,13 +5,9 @@ import TrekPackageReducer from '../pages/trek/trekSlice';
 
 export const store = configureStore({
     reducer: {
-<<<<<<< HEAD
-        navigationList: NavigationListReducer
-=======
         navigationList: NavigationListReducer,
         skiPackage: SkiPackageReducer,
         trekPackage: TrekPackageReducer,
->>>>>>> 3c2d9902d29b389b3eccda9acd29f886cf6bcad3
     },
 })
 
